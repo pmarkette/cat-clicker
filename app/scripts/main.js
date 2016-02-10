d476db0456c8c6b4bbd4821344624168dcb9cfc2
pmarkette <paul@markette.net>
2016-02-09 22:42:15 -0800

    Add names

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,24 @@
 /*eslint-env jquery */
+var names = ["Chester", "Tony"];
+
+var $catName0 = $("#catName0");
+$catName0.html(names[0]);
 var count0 = 0;
 var $clickCounter0 = $("#click-counter0");
 $("#catpic0").click(function() {
 	"use strict";
   count0++;
-  $clickCounter0.html("You have clicked this cat " + count0 + " times!");
+  $clickCounter0.html("You have clicked " + names[0] + " " + count0 + " times!");
 });
 
+var $catName1 = $("#catName1");
+$catName1.html(names[1]);
 var count1 = 0;
 var $clickCounter1 = $("#click-counter1");
 $("#catpic1").click(function() {
 	"use strict";
   count1++;
-  $clickCounter1.html("You have clicked this cat " + count1 + " times!");
+  $clickCounter1.html("You have clicked " + names[1] + " " + count1 + " times!");
 });
+
+
