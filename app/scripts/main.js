f699f25d9ecbee841c241528dc6a84d4e8886c3f
pmarkette <paul@markette.net>
2016-02-09 22:07:40 -0800

    Add second cat picture. Duplicate code for 2 cats.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,16 @@
 /*eslint-env jquery */
-var count = 0;
-var $clickCounter = $("#click-counter");
-$("#catpic").click(function() {
+var count0 = 0;
+var $clickCounter0 = $("#click-counter0");
+$("#catpic0").click(function() {
 	"use strict";
-  count++;
-  $clickCounter.html("You have clicked this cat " + count + " times!");
+  count0++;
+  $clickCounter0.html("You have clicked this cat " + count0 + " times!");
+});
+
+var count1 = 0;
+var $clickCounter1 = $("#click-counter1");
+$("#catpic1").click(function() {
+	"use strict";
+  count1++;
+  $clickCounter1.html("You have clicked this cat " + count1 + " times!");
 });
